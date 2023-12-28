7d30b255411b08ffb4dd57dc9798f0b9d1d21a63
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2023-12-27 23:54:18 -0500

    initial commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '65850a27ecc6cc6aff0f';
+  static const String projectId = '65850936e0b70605723d';
+  static const String endPoint = 'http://localhost:80/v1';
+}
